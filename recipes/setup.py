43da277cd59a904e90b4c86616b5b5eae7d8c016
12138mICHAEL1111 <1158969792@qq.com>
2024-01-16 15:47:45 +0000

    feat: Add redis for result collection

--- recipes/setup.py
+++ recipes/setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name="euphrosyne-recipes",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "requests",
+        "redis",
     ],
     entry_points={
         "console_scripts": [
             "dummy = scripts.dummy:main",
+            "demo = scripts.demo:main",
         ],
     },
 )
--- reconciler/recipe_executor.go
+++ reconciler/recipe_executor.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"go.uber.org/zap"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/yaml"
-
-	"go.uber.org/zap"
 )
 
 const (
@@ -29,14 +32,30 @@ type RecipeConfig struct {
 }
 
 // Initialise and run the recipe executor.
-func StartRecipeExecutor(alertData *map[string]interface{}, clientset *kubernetes.Clientset, logger *zap.Logger) {
-
+func StartRecipeExecutor(c *gin.Context, alertData *map[string]interface{}, clientset *kubernetes.Clientset, logger *zap.Logger, rdb *redis.Client) {
 	// Retrieve recipes from ConfigMap
 	recipes, err := getRecipesFromConfigMap(clientset)
 	if err != nil {
 		logger.Error("Failed to retrieve recipes from ConfigMap", zap.Error(err))
 		return
 	}
+
+	// using alertId as the unique channel name, 
+	// will be changed based on real alert json struct
+	alertId, ok := (*alertData)["alertId"].(string)
+    if !ok {
+        logger.Error("No alertId field in alert json or it is not a string", zap.Error(err))
+		return
+    }
+
+	pubsub := rdb.Subscribe(c,alertId)
+
+	_, err = pubsub.Receive(c)
+
+    if err != nil {
+		logger.Error("failed to subscribe channel", zap.Error(err))
+		return
+    }
 
 	// Create a Job for each recipe
 	for recipeName, recipeConfig := range recipes {
@@ -47,7 +66,42 @@ func StartRecipeExecutor(alertData *map[string]interface{}, clientset *kubernete
 		}
 	}
 
+    ch := pubsub.Channel()
+
+	messageCount := 0
+
+	timeoutDuration := 10 * time.Second
+	timeout := time.NewTimer(timeoutDuration)
+	shouldBreak := false
+
+	for{
+		select {
+			case msg := <-ch:
+				fmt.Println(msg.Channel, msg.Payload)
+				messageCount++
+				if messageCount == len(recipes) {
+					shouldBreak = true 
+				}
+			// if not enough messages received in 10 seconds, close channel
+			// means running error in recipe, fail to compile, or internal code error
+			case <-timeout.C:
+				shouldBreak = true 
+				logger.Warn("Not enough message received in 10 seconds, closing channel. ")
+		}
+		if shouldBreak {
+			break
+		}
+	}
+
+	err = pubsub.Close()
+    if err != nil {
+        logger.Error("Failed to close channel", zap.Error(err))
+		return
+    }
+
 	logger.Info("Recipe executor initialisation completed")
+
+
 }
 
 // Retrieve recipes from ConfigMap.
@@ -79,7 +133,7 @@ func getRecipesFromConfigMap(clientset *kubernetes.Clientset) (map[string]Recipe
 // Create a Kubernetes Job to execute a recipe.
 func createJob(
 	clientset *kubernetes.Clientset, recipeName string, recipeConfig RecipeConfig, alertData *map[string]interface{},
-) error {
+) error{
 	jobClient := clientset.BatchV1().Jobs(jobNamespace)
 
 	// Define the Job object
@@ -114,25 +168,32 @@ func createJob(
 	}
 
 	_, err := jobClient.Create(context.TODO(), job, metav1.CreateOptions{})
+
 	if err != nil {
 		return err
 	}
 
 	logger.Info("Job created successfully", zap.String("jobName", job.Name))
-	return nil
+
+	return  nil
 }
 
 // Build Recipe command
 func buildRecipeCommand(recipeConfig RecipeConfig, alertData *map[string]interface{}) string {
-	var alertDataString string
-	for _, value := range *alertData {
-		alertDataString += fmt.Sprintf("%v ", value)
-	}
+	// var alertDataString string
+	// for _, value := range *alertData {
+	// 	alertDataString += fmt.Sprintf("%v ", value)
+	// }
+	
+	alertDataStr, err := json.Marshal(alertData)
+    if err != nil {
+		logger.Error("failed to convert alertData to string", zap.Error(err))
+    }
 
 	var recipeCommand string
 	recipeCommand += fmt.Sprintf("%v ", recipeConfig.Entrypoint)
 	for _, param := range recipeConfig.Params {
-		recipeCommand += fmt.Sprintf("--%v '%v'", param.Name, alertDataString)
+		recipeCommand += fmt.Sprintf("--%v '%v'", param.Name, string(alertDataStr))
 	}
 	return recipeCommand
 }
