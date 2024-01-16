72230f99e5a76b7393013fdadf1c8d103d728d31
12138mICHAEL1111 <1158969792@qq.com>
2024-01-16 15:47:45 +0000

    Add redis for result collection

--- reconciler/alert_handler.go
+++ reconciler/alert_handler.go
@@ -4,20 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
 	"k8s.io/client-go/kubernetes"
 )
 
-func StartAlertHandler(clientset *kubernetes.Clientset, logger *zap.Logger) {
+func StartAlertHandler(clientset *kubernetes.Clientset, logger *zap.Logger, rdb *redis.Client) {
 	router := gin.Default()
-	router.POST("/webhook", func(ctx *gin.Context) { handleWebhook(ctx, clientset, logger) })
+	router.POST("/webhook", func(ctx *gin.Context) { handleWebhook(ctx, clientset, logger,rdb) })
 
 	if err := router.Run(":8080"); err != nil {
 		logger.Error("Failed to start server", zap.Error(err))
 	}
 }
 
-func handleWebhook(c *gin.Context, clientset *kubernetes.Clientset, logger *zap.Logger) {
+func handleWebhook(c *gin.Context, clientset *kubernetes.Clientset, logger *zap.Logger,rdb *redis.Client) {
 	var alertData map[string]interface{}
 
 	if err := c.BindJSON(&alertData); err != nil {
@@ -30,7 +31,7 @@ func handleWebhook(c *gin.Context, clientset *kubernetes.Clientset, logger *zap.
 	logger.Info("Alert received", zap.Any("alert", alertData))
 
 	// Start the recipe executor
-	go StartRecipeExecutor(&alertData, clientset, logger)
+	go StartRecipeExecutor(c, &alertData, clientset, logger,rdb)
 
 	c.JSON(http.StatusOK, gin.H{"message": "Alert received and processed"})
 }
--- reconciler/go.mod
+++ reconciler/go.mod
@@ -4,9 +4,11 @@ go 1.21.5
 
 require (
 	github.com/bytedance/sonic v1.10.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/emicklei/go-restful/v3 v3.11.2 // indirect
 	github.com/evanphx/json-patch v5.7.0+incompatible // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
@@ -19,6 +21,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
