b9b8721db6f157662096cb4f231e994310ae54d9
Phoevos Kalemkeris <phoevos@protonmail.com>
2024-02-04 15:29:11 +0000

    sdk: Use the incident abstraction for recipe runs
    Signed-off-by: Phoevos Kalemkeris <phoevos@protonmail.com>

--- recipes/scripts/dummy.py
+++ recipes/scripts/dummy.py
@@ -1,13 +1,14 @@
 import logging
 
+from sdk.incident import Incident
 from sdk.recipe import Recipe, RecipeResults, RecipeStatus
 
 logger = logging.getLogger(__name__)
 
 
-def handler(data: dict, results: RecipeResults):
+def handler(incident: Incident, results: RecipeResults):
     """Dummy Recipe."""
-    logger.info("Received input:", data)
+    logger.info("Received input:", incident)
     results.log("Tough luck mate!")
     results.status = RecipeStatus.SUCCESSFUL
     return results
