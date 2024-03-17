3fba7239575cae6fbaac19318011f6067da8995d
Isabella Liu <1749553911@qq.com>
2024-03-17 20:09:08 +0000

    move HttpError code to ServiceError

--- recipes/sdk/errors.py
+++ recipes/sdk/errors.py
@@ -1,49 +1,26 @@
 import requests
 
-
-#TODO: move implementation to the base class
 class ServiceError(requests.exceptions.RequestException):
     """Base class for external service errors."""
+    def __init__(self, original_exception):
+        self.original_exception = original_exception
 
+    def to_dict(self):
+        return {
+            "error": {
+                "type": type(self.original_exception).__name__,
+                "message": str(self.original_exception),
+            }
+        }
+
+class DataAggregatorHTTPError(ServiceError):
     pass
 
-#QUESTION: 7. three http errors have the same structure
-# maybe we can move all parts in the base class
-class DataAggregatorHTTPError(ServiceError):
-    def __init__(self, original_exception):
-        self.original_exception = original_exception
-
-    def to_dict(self):
-        return {
-            "error": {
-                "type": type(self.original_exception).__name__,
-                "message": str(self.original_exception),
-            }
-        }
-
 class SpaceHTTPError(ServiceError):
-    def __init__(self, original_exception):
-        self.original_exception = original_exception
-
-    def to_dict(self):
-        return {
-            "error": {
-                "type": type(self.original_exception).__name__,
-                "message": str(self.original_exception),
-            }
-        }
+    pass
 
 class JiraHTTPError(ServiceError):
-    def __init__(self, original_exception):
-        self.original_exception = original_exception
-
-    def to_dict(self):
-        return {
-            "error": {
-                "type": type(self.original_exception).__name__,
-                "message": str(self.original_exception),
-            }
-        }
+    pass
 
 
 class SpaceParsingError(ValueError):
